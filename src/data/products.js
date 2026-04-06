d6ae042cf775a433b85e68a5f19e01ebb27bc524
medaboinacharan-pixel <medaboina.charan.s.138@kalvium.community>
2026-04-06 21:04:52 +0530

    Setup React app using Vite

---
+++ src/data/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 14",
+    price: "₹79,999",
+    category: "Electronics",
+    description: "Latest Apple phone",
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "T-Shirt",
+    price: "₹999",
+    category: "Clothing",
+    description: "Comfort cotton t-shirt",
+    image: "https://via.placeholder.com/150"
+  },
+  // add total 8 products
+];
+
+export default products;
